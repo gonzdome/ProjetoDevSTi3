549add8061fa2c6d52f16dd65c50b5af95d9fd30
Gabriel Gonzalez Domeniconi <gabrielgdomeniconi@gmail.com>
2021-07-19 19:32:34 -0300

    UserControl: Detalhes do Pedido adicionado estéticamente;

---
+++ ProjetoDevSTi3/View/UserControls/UcOrderDetails.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace ProjetoDevSTi3.View.UserControls
+{
+    /// <summary>
+    /// Interaction logic for UcOrderDetails.xaml
+    /// </summary>
+    public partial class UcOrderDetails : UserControl
+    {
+        public UcOrderDetails()
+        {
+            InitializeComponent();
+        }
+    }
+}
