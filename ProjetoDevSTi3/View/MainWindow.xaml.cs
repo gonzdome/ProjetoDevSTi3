9f1219034261fa47a175b07132946b5133f3a56f
Gabriel Gonzalez Domeniconi <gabrielgdomeniconi@gmail.com>
2021-07-21 18:11:48 -0300

    Botão para fechar UcOrderDetails adicionado;

--- ProjetoDevSTi3/View/MainWindow.xaml.cs
+++ ProjetoDevSTi3/View/MainWindow.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Windows;
+﻿using ProjetoDevSTi3.View.UserControls;
+using System;
+using System.Windows;
+using System.Windows.Controls;
 
 namespace ProjetoDevSTi3.View
 {
@@ -10,10 +13,26 @@ namespace ProjetoDevSTi3.View
         public MainWindow()
         {
             InitializeComponent();
+            BtnFechar.IsEnabled = false;
         }
         private void BtnPesquisar_Click(object sender, RoutedEventArgs e)
         {
-            
+            InicializarUc(sender);
+        }
+
+        private void InicializarUc(object sender)
+        {
+            if (sender is Button)
+            {
+                Conteudo.Content = new UcOrderDetails();
+                BtnFechar.IsEnabled = true;
+            }
+        }
+
+        private void BtnFechar_Click(object sender, EventArgs e)
+        {
+            Conteudo.Content = "";
+            BtnFechar.IsEnabled = false;
         }
 
         private void BtnSincronizar_Click(object sender, RoutedEventArgs e)
