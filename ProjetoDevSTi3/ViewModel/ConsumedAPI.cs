da4a8994586e92b14f26bedebfa277f533d0b72a
Gabriel Gonzalez Domeniconi <gabrielgdomeniconi@gmail.com>
2021-07-22 20:02:44 -0300

    Criando classe para receber dados da API;

---
+++ ProjetoDevSTi3/ViewModel/ConsumedAPI.cs
@@ -0,0 +1,38 @@
+﻿using Azure;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Windows.Controls;
+
+namespace ProjetoDevSTi3.ViewModel
+{
+    public class ConsumedAPI
+    {
+        public long Id { get; set; }
+        public long Numero { get; set; }
+        public DateTime DataCriacao { get; set; }
+        public DateTime DataAlteracao { get; set; }
+        public string Status { get; set; }
+        public decimal Desconto { get; set; }
+        public decimal Frete { get; set; }
+        public decimal SubTotal { get; set; }
+        public decimal Total { get; set; }
+
+
+        public string Nome { get; set; }
+        public int Documento { get; set; }
+        public DateTime DataNascimento { get; set; }
+        public string Email { get; set; }
+
+
+        public string Endereco { get; set; }
+        public int NumeroRua { get; set; }
+        public string Cep { get; set; }
+        public string Bairro { get; set; }
+        public string Cidade { get; set; }
+        public string Estado { get; set; }
+        public string Complemento { get; set; }
+        public string Referencia { get; set; }
+    }
+}
