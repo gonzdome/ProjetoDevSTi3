c8422b14a8cc824807ad38303570812aa7b60079
Gabriel Gonzalez Domeniconi <gabrielgdomeniconi@gmail.com>
2021-07-27 17:50:01 -0300

    API consumida aparentemente com sucesso;

--- ProjetoDevSTi3/View/MainWindow.xaml.cs
+++ ProjetoDevSTi3/View/MainWindow.xaml.cs
@@ -82,26 +82,47 @@ namespace ProjetoDevSTi3.View
             public string SubTotal { get; set; }
             public string ValorTotal { get; set; }
 
-            public string Nome { get; set; }
-            public string DataNascimento { get; set; }
-            public string Email { get; set; }
-            public string Cnpj { get; set; }
-            public string Cpf { get; set; }
-            public string RazaoSozial { get; set; }
+            public class Cliente
+            {
+                public string Id { get; set; }
+                public string Nome { get; set; }
+                public string DataNascimento { get; set; }
+                public string Email { get; set; }
+                public string Cnpj { get; set; }
+                public string Cpf { get; set; }
+                public string RazaoSozial { get; set; }
+            }
 
+            public class EnderecoEntrega
+            {
+                public string Id { get; set; }
+                public string Endereco { get; set; }
+                public string Numero { get; set; }
+                public string Cep { get; set; }
+                public string Bairro { get; set; }
+                public string Cidade { get; set; }
+                public string Estado { get; set; }
+                public string Complemento { get; set; }
+                public string Referencia { get; set; }
+            }
 
-            //public string IdProduto { get; set; }
-            //public string Quantidade { get; set; }
-            //public string Parcela { get; set; }
-            //public string Codigo { get; set; }
-            //public string Endereco { get; set; }
-            //public string NumeroRua { get; set; }
-            //public string Cep { get; set; }
-            //public string Bairro { get; set; }
-            //public string Cidade { get; set; }
-            //public string Estado { get; set; }
-            //public string Complemento { get; set; }
-            //public string Referencia { get; set; }
+            public class Itens
+            {
+                public string Id { get; set; }
+                public string IdProduto { get; set; }
+                public string Nome { get; set; }
+                public string Quantidade { get; set; }
+                public string ValorUnitario { get; set; }
+            }
+
+            public class Pagamento
+            {
+                public string Id { get; set;}
+                public string Parcela { get; set;}
+                public string Valor { get; set;}
+                public string Codigo { get; set;}
+                public string Nome { get; set;}
+            }
         }
     }
 }
